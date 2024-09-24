843d901dabe833c69b1d59ed21e625e120bf492d
hoerudin14 <ptc.hoerudin@gmail.com>
2024-09-24 14:25:01 +0700

    update sonar

--- app.js
+++ app.js
@@ -23,11 +23,6 @@ app.get('/app2', (req, res) => {
 app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
-    if {
-      console.log('error', error)
-    } else {
-      res.send(fields)
-    }
   })
 });
 
